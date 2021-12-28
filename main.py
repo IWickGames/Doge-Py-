a2c4f196b9256b3ac163555079deff4d030cb596
IWickGames <smarttecgaming@gmail.com>
2021-12-27 20:50:40 -0600

    Added __pycache__ cleanup after databace flush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import groups
 import config
 import discord
@@ -25,3 +26,10 @@ except:  # noqa: E722
     bot.loop.run_until_complete(bot.close())
 finally:
     db.databace.Flush()
+
+    if os.path.exists("__pycache__"):
+        print("Cleaning up...")
+        shutil.rmtree("__pycache__")
+        for folder in config.cog_directorys:
+            shutil.rmtree(folder + "/__pycache__")
+        shutil.rmtree("db/__pycache__")
