45ab091526a1aff8136af5ce1650060572808584
IWickGames <smarttecgaming@gmail.com>
2021-12-27 22:54:27 -0600

    Added view command

---
+++ settings/view.py
@@ -0,0 +1,74 @@
+import groups
+import config
+import db.sets
+import db.databace
+from discord.ext import commands
+from discord.commands.commands import Option
+
+
+class View(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @groups.settings.command()
+    async def view(
+        ctx: commands.Context,
+        type: Option(
+            str,
+            description="Config type User or Guild",  # noqa: F722
+            required=True,
+            choices=["User", "Guild"]  # noqa: F821
+        ),
+        setting: Option(
+            str,
+            description="The setting you wish to view",  # noqa: F722
+            required=True
+        )
+    ):
+        """View a configuration value"""
+
+        match type:
+            case "User":
+                if setting not in db.sets.user_settings:
+                    await ctx.respond(
+                        f"Invalid user setting `{setting}`",
+                        ephemeral=True
+                    )
+                    return
+                value = await db.databace.ReadKey(
+                    f"settings.{ctx.author.id}.{setting}"
+                )
+            case "Guild":
+                if not ctx.author.guild_permissions.administrator:
+                    await ctx.respond(
+                        config.bot_permission_errormsg,
+                        ephemeral=True
+                    )
+                    return
+
+                if setting not in db.sets.guild_settings:
+                    await ctx.respond(
+                        f"Invalid guild setting `{setting}`",
+                        ephemeral=True
+                    )
+                    return
+                value = await db.databace.ReadKey(
+                    f"settings.{ctx.guild.id}.{setting}"
+                )
+
+        if not value:
+            await ctx.respond(
+                "That setting is not currently "
+                "defined and is using default values",
+                ephemeral=True
+            )
+            return
+
+        await ctx.respond(
+            f"The value of {setting} is ```{value}```",
+            ephemeral=True
+        )
+
+
+def setup(bot):
+    bot.add_cog(View(bot))
