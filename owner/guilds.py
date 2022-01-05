4107a8c48cdfb15e6707d10bfba42b5bd6df2bbf
iwickgames <smarttecgaming@gmail.com>
2022-01-05 17:36:08 -0600

    Working on guilds command

---
+++ owner/guilds.py
@@ -0,0 +1,29 @@
+import config
+import groups
+import discord
+from discord.ext import commands
+
+
+class Guilds(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @groups.owner.command(guild_ids=config.test_servers)
+    async def guilds(ctx: commands.Context):
+        """List the bots guilds"""
+        if ctx.author.id not in config.authorized_users:
+            await ctx.respond(config.bot_permission_errormsg, ephemeral=True)
+            return
+            
+        lines = [f"""{g.name} ({g.id})\n""" for g in config.bot.guilds]
+        
+        emb = discord.Embed(
+            title="Joined guilds",
+            description=lines,
+            color=config.embed_color
+        )
+        ctx.respond(embed=emb, ephemeral=True)
+
+
+def setup(bot):
+    bot.add_cog(Guilds(bot))
