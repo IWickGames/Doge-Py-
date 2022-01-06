650f4e9b5586f1498ef838805a16572edbf759f9
IWickGames <smarttecgaming@gmail.com>
2022-01-06 16:26:30 -0600

    Update token missing messages

--- config.py
+++ config.py
@@ -13,16 +13,19 @@ def PassBot(b: discord.Bot):
 if os.getenv("token"):
     token = os.getenv("token")
 else:
+    if not os.path.exists("token.env"):
+        log.logging.SyncError("Unable to locate environment token or token file")
+        sys.exit(1)
     try:
         token = open("token.env").read()
     except OSError:
-        log.logging.SyncError("Unable to locate token file or token environment entry")
+        log.logging.SyncError("Unable to read token file")
         sys.exit(1)
 
 authorized_users = [320699339645124608]
 
-test_servers = [735935481950503043, 673399675818213385]
-# test_servers = None
+# test_servers = [735935481950503043, 673399675818213385]
+test_servers = None
 databace_file = "db/bot.db"
 cog_directorys = [
     "tickets",
