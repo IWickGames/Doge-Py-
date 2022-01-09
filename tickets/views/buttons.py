0c03c3f7e68eefd2d2d46367ee016e41dc041662
IWickGames <smarttecgaming@gmail.com>
2022-01-09 13:00:53 -0600

    Added changed ticket behavior

---
+++ tickets/views/buttons.py
@@ -0,0 +1,69 @@
+import discord
+
+
+class CloseBtn(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(
+        label="🔒 Close Ticket",
+        style=discord.ButtonStyle.red
+    )
+    async def close(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        tnum = interaction.channel.name.split("-")[-1]
+
+        try:
+            await interaction.channel.edit(
+                name=f"closed-{tnum}"
+            )
+        except discord.Forbidden:
+            await interaction.response.send_message(
+                ":closed_lock_with_key: Bot does not"
+                " have permissions to edit this channel",
+                ephemeral=True
+            )
+            return
+
+        await interaction.response.send_message(
+            ":lock: Ticket closed by "
+            f"`{interaction.user.name}#{interaction.user.discriminator}`",
+            view=ReopenBtn()
+        )
+
+        self.stop()
+
+
+class ReopenBtn(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(
+        label="🔓 Reopen Ticket",
+        style=discord.ButtonStyle.green
+    )
+    async def close(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        tnum = interaction.channel.name.split("-")[-1]
+
+        try:
+            await interaction.channel.edit(
+                name=f"ticket-{tnum}"
+            )
+        except discord.Forbidden:
+            await interaction.response.send_message(
+                ":closed_lock_with_key: Bot does not"
+                " have permissions to edit this channel",
+                ephemeral=True
+            )
+            return
+
+        await interaction.response.send_message(
+            "🔓 Ticket reopend by "
+            f"`{interaction.user.name}#{interaction.user.discriminator}`",
+            view=CloseBtn()
+        )
+
+        self.stop()
