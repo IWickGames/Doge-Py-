d6880115ee82b7a4977499a6fd237954c9f509b2
iwickgames <smarttecgaming@gmail.com>
2022-01-19 11:26:14 -0600

    Using config to pass bot instead of function globals

--- groups.py
+++ groups.py
@@ -1,56 +1,47 @@
-def MakeGroups(bot):
-    global tricks
-    global moderation
-    global utilities
-    global fun
-    global leveling
-    global settings
-    global tickets
-    global economy
-    global owner
+import config
 
-    tricks = bot.create_group(
-        name="tricks",
-        description="My trick set"
-    )
+tricks = config.bot.create_group(
+    name="tricks",
+    description="My trick set"
+)
 
-    moderation = bot.create_group(
-        name="moderation",
-        description="Only authorized people here"
-    )
+moderation = config.bot.create_group(
+    name="moderation",
+    description="Only authorized people here"
+)
 
-    utilities = bot.create_group(
-        name="utilities",
-        description="Some usefull things"
-    )
+utilities = config.bot.create_group(
+    name="utilities",
+    description="Some usefull things"
+)
 
-    fun = bot.create_group(
-        name="fun",
-        description="The fun things"
-    )
+fun = config.bot.create_group(
+    name="fun",
+    description="The fun things"
+)
 
-    leveling = bot.create_group(
-        name="leveling",
-        description="Leveling system"
-    )
+leveling = config.bot.create_group(
+    name="leveling",
+    description="Leveling system"
+)
 
-    settings = bot.create_group(
-        name="settings",
-        description="Manage user and guild settings"
-    )
+settings = config.bot.create_group(
+    name="settings",
+    description="Manage user and guild settings"
+)
 
-    tickets = bot.create_group(
-        name="tickets",
-        description="Ticketing feature for users "
-        "to create tickets on your guild"
-    )
+tickets = config.bot.create_group(
+    name="tickets",
+    description="Ticketing feature for users "
+    "to create tickets on your guild"
+)
 
-    economy = bot.create_group(
-        name="economy",
-        description="Show me the moneeeeeeeey"
-    )
+economy = config.bot.create_group(
+    name="economy",
+    description="Show me the moneeeeeeeey"
+)
 
-    owner = bot.create_group(
-        name="owner",
-        description="Bot owner commands"
-    )
+owner = config.bot.create_group(
+    name="owner",
+    description="Bot owner commands"
+)
